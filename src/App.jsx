8821166e69c32542942e8721cc1573e69eb7f80f
Yabets solomon <jabezsolomonz@gmail.com>
2024-11-12 16:58:36 +0300

    initializing the new repository

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import ChangeDecimal from './components/changeDecimal'
+
+function App() {
+  
+
+  return (
+    <>
+
+    <ChangeDecimal/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/changeDecimal.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./changeDecimal.css"
+
+function ChangeDecimal() {
+  const [decimal, setDecimal] = useState();
+  const [binary, setBinary] = useState("");
+
+  const handleDecimal = (e) => {
+    const value = parseInt(e.target.value, 10);
+    if (!isNaN(value)) {
+      setDecimal(value); 
+    } else {
+      
+      console.error("Invalid input: Not a number");
+    }
+  };
+
+  const change = (number) => {
+    let result = [];
+    while (number >= 1) {
+      if (number % 2 === 0) {
+        result.push(0);
+        number = number / 2;
+      } else {
+        result.push(1);
+        number = (number - 1) / 2;
+      }
+    }
+    result.reverse();
+    return result.join("");
+  };
+
+  const handleConvert = () => {
+    setBinary(change(decimal));
+  };
+
+  return (
+    <div>
+    <h1>Convert Decimal to Binary</h1>
+      <div className="binary-to-decimal-container">
+        
+        <input
+          className="input-number"
+          type="number"
+          value={decimal}
+          onChange={(e) => handleDecimal(e)}
+        />
+        <button onClick={handleConvert}>Convert</button>
+      </div>
+      <p>{binary}</p> {/* Display the result here */}
+    </div>
+  );
+}
+
+export default ChangeDecimal;
